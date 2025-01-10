1753dcfcdb27d2d6672383042ab0834e4bd78964
HardBoss07 <matteo@bosshard.net>
2025-01-10 12:47:04 +0100

    fixed frontend not returning a react component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google"; // Import Roboto font
 import "./globals.css";
+
+// Roboto Regular font
+const roboto = Roboto({
+    weight: "400",
+    subsets: ["latin"],
+    variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+    title: "NovaTech",
+    description: "Welcome to NovaTech",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <body className={`${roboto.variable} antialiased`}>
+        {children}
+        </body>
+        </html>
+    );
+}
