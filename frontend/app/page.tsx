1453b96ca6936bfe4e61217db92c7fdb399b472b
HardBoss07 <matteo@bosshard.net>
2025-01-10 09:15:27 +0100

    init frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
-    return (
-        <main>
-            <h1>Fullstack Application Template</h1>
-        </main>
-    );
+  return (
+    <main>
+      <h1>FULLSTACK TEMPLATE</h1>
+    </main>
+  );
 }
